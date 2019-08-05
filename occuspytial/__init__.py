0f2ad9b2c1e4ef7a9ea4e25938777d92d177fba0
RedRuM <44142765+zoj613@users.noreply.github.com>
2019-08-05 09:24:26 +0200

    clean up init modules

--- occuspytial/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import icar
-from . import utils
-
-__all__ = ["icar", "utils"]
--- occuspytial/utils/__init__.py
+++ occuspytial/utils/__init__.py
@@ -1,19 +1,1 @@
-from .utils import (
-    SpatialStructure,
-    ProgressBar,
-    affine_sample,
-    acf,
-    CustomDict,
-    log_config
-)
-from .basemodel import MCMCModelBase
 
-__all__ = [
-    "SpatialStructure",
-    "ProgressBar",
-    "affine_sample",
-    "acf",
-    "CustomDict",
-    "MCMCModelBase",
-    "log_config"
-]
