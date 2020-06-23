edac0c15d632b66e1a3fbc1cdcbeb0228127e81d
RedRuM <44142765+zoj613@users.noreply.github.com>
2020-06-23 21:31:45 +0200

    EHN: Implement Gibbs sampler for Spatial occupancy model using the Logit link and RSR and ICAR models as distributions of the prior of the spatial parameter

---
+++ occuspytial/gibbs/base.py
@@ -0,0 +1,196 @@
+from abc import ABC, abstractmethod
+from copy import deepcopy
+
+import numpy as np
+from scipy.sparse import csc_matrix, isspmatrix_csc, issparse
+from scipy.sparse.linalg import eigsh
+from tqdm import tqdm
+
+from ..chain import Chain
+from ..data import Data
+from ..posterior import PosteriorParameter
+
+from .parallel import sample_parallel
+from .state import State, FixedState
+
+
+class _GibbsState(State):
+    _posterior_names = ('alpha', 'beta', 'tau', 'z', 'eta')
+
+    @property
+    def posteriors(self):
+        return {key: self.__dict__[key] for key in self._posterior_names}
+
+
+class GibbsBase(ABC):
+    def __init__(self, Q, W, X, y, hparams=None, random_state=None):
+        self.W = Data(W)
+        self.X = X
+        self.y = Data(y)
+        self.chain = None
+        self.random_state = random_state
+        self.rng = np.random.default_rng(random_state)
+
+    @abstractmethod
+    def step(self):
+        pass
+
+    def _configure(self, Q, hparams, verify_precision=True,**kwargs):
+        if verify_precision:
+            self._verify_spatial_precision(Q)
+
+        self.state = _GibbsState()
+        self.state.z = np.ones(self.X.shape[0])
+        self.state.z[self.y.surveyed] = [
+            0 if not any(self.y[site]) else 1 for site in self.y.surveyed
+        ]
+        self.state.k = self.state.z - 0.5
+
+        self.fixed = FixedState()
+        self.fixed.n = self.X.shape[0]
+        self.fixed.ones = np.ones(self.fixed.n)
+        self.fixed.zeros = np.zeros(self.fixed.n)
+        self.fixed.not_surveyed = [
+            site for site in range(self.fixed.n) if site not in self.y.surveyed
+        ]
+        self.fixed.not_obs = (self.state.z[self.W.surveyed] == 0).nonzero()[0]
+        self.fixed.W_not_obs = self.W[self.fixed.not_obs]
+        self.fixed.visits_not_obs = self.W.visits(self.fixed.not_obs)
+        self.fixed.visits = [
+            self.W.data[i].shape[0] for i in self.y.surveyed
+        ]
+        sections = np.cumsum(self.fixed.visits_not_obs)
+        self.fixed.sections = np.pad(sections, (1, 0))
+        self.state.section_sums = np.zeros(sections.shape[0], dtype=np.double)
+        self.fixed.Q = Q if isspmatrix_csc(Q) else csc_matrix(Q)
+
+        if hparams:
+            self.fixed = self._set_hyperparams(self.fixed, hparams)
+        else:
+            self.fixed = self._set_default_hyperparams(self.fixed)
+        self.fixed.a_prec_by_mu = self.fixed.a_prec @ self.fixed.a_mu
+        self.fixed.b_prec_by_mu = self.fixed.b_prec @ self.fixed.b_mu
+        self.fixed.zeros_no = self.fixed.zeros[self.fixed.not_obs]
+        self.fixed.zeros_ns = self.fixed.zeros[self.fixed.not_surveyed]
+
+        self.dists = FixedState()
+
+    def _verify_spatial_precision(self, Q):
+        smallest_eigv = eigsh(Q, k=1, which='SA', return_eigenvectors=False)[0]
+        if smallest_eigv >= 1e-4:
+            raise ValueError('Spatial precision matrix Q must be singular.')
+
+    def _set_hyperparams(self, params, hyperparams):
+        for key, value in hyperparams.items():
+            params[key] = value
+        return params
+
+    def _set_default_hyperparams(self, params):
+        params.tau_rate = 0.005
+        params.tau_shape = 0.5 + 0.5 * (self.fixed.n - 1)
+        alpha_size = self.W[0].shape[1]
+        params.a_mu = np.zeros(alpha_size)
+        params.a_prec = np.eye(alpha_size) / 10
+        beta_size = self.X.shape[1]
+        params.b_mu = np.zeros(beta_size)
+        params.b_prec = np.eye(beta_size) / 10
+        return params
+
+    def _initialize_posterior_state(self, start=None):
+        if start is None:
+            self.state = self._initialize_default_start(self.state)
+        else:
+            self.state.alpha = start['alpha']
+            self.state.beta = start['beta']
+            self.state.tau = start['tau']
+            self.state.eta = start['eta']
+            self.state.spatial = self.state.eta
+
+    def _initialize_default_start(self, state):
+        state.tau = self.rng.gamma(0.5, 1 / self.fixed.tau_rate)
+        eta = self.rng.standard_normal(self.fixed.n)
+        eta[-1] = -eta[:-1].sum()
+        state.eta = eta
+        state.spatial = self.state.eta
+        state.alpha = self.rng.multivariate_normal(
+            self.fixed.a_mu, 100 * self.fixed.a_prec
+        )
+        state.beta = self.rng.multivariate_normal(
+            self.fixed.b_mu, 100 * self.fixed.b_prec
+        )
+        return state
+
+    def _run(
+        self, size, burnin=0, start=None, chains=1, progressbar=True, pos=0
+    ):
+        self._initialize_posterior_state(start)
+        chain_params = {
+            'alpha': self.state.alpha.size,
+            'beta':  self.state.beta.size,
+            'eta':  self.state.eta.size,
+            'z':  self.fixed.n,
+            'tau': 1
+        }
+        self.chain = Chain(chain_params, size - burnin)
+        tqdm_iterator = tqdm(
+            range(size), total=size, disable=not progressbar, position=pos
+        )
+        for i in tqdm_iterator:
+            self.step()
+            if i >= burnin:
+                self.chain.append(self.state.posteriors)
+
+        return self.chain
+
+    def sample(
+        self, size, burnin=0, start=None, chains=1, progressbar=True, pos=0
+    ):
+        if burnin >= size:
+            raise ValueError('burnin value cannot be larger than sample size')
+        if chains < 1:
+            raise ValueError('chains must a postive integer.')
+
+        samples = sample_parallel(
+            self, size=size, burnin=burnin, chains=chains, start=start
+        )
+        out = PosteriorParameter(*samples)
+        return out
+
+    def resample(self, chain, size):
+        # TODO: clean this up and improve efficiency
+        if not isinstance(chain, PosteriorParameter):
+            raise ValueError(
+                '"chain" needs to be an instance of PosteriorParameter'
+            )
+        chain = deepcopy(chain)
+        out = chain.data.pad(
+            pad_width={'draw': (0, size)}, mode='constant', constant_values=0
+        )
+        out['draw'] = np.arange(out.draw.size)
+        # pad the exluded params
+        for key in chain._excluded:
+            chain._excluded[key] = np.pad(
+                chain._excluded[key], [(0, 0), (0, size), (0, 0)]
+            )
+        for i in range(len(chain.data.chain)):
+            start = {
+                key: val[i][-(size + 1)]
+                for key, val in chain._excluded.items()
+            }
+            start.update({
+                k: chain[k][i][-1] for k in chain.data.keys()
+            })
+            new_chain = self.sample(size, burnin=0, start=start)
+            for key in chain.data.keys():
+                out[key].data[i][-size:] = new_chain[key]
+            for key, val in chain._excluded.items():
+                chain._excluded[key][i][-size:] = new_chain._excluded[key][0]
+        chain.data = out
+        return chain
+
+    def copy(self):
+        out = type(self).__new__(self.__class__)
+        out.__dict__.update(self.__dict__)
+        # make sure the copy has its own unique random number generator
+        out.__dict__['rng'] = np.random.default_rng()
+        return out
---
+++ occuspytial/gibbs/logit.py
@@ -0,0 +1,183 @@
+import numpy as np
+from numpy.linalg import multi_dot
+from scipy.linalg import solve_triangular, eigh
+from scipy.sparse.linalg import eigsh
+
+from ..distributions import (
+    PolyaGamma,
+    SumToZeroMultivariateNormal,
+    DenseMultivariateNormal2,
+)
+
+from .base import GibbsBase
+from ._cumsum import split_sum
+
+
+class LogitICARGibbs(GibbsBase):
+
+    def __init__(self, Q, W, X, y, hparams=None, random_state=None, pertub=None):
+        super().__init__(Q, W, X, y, hparams, random_state)
+        self._configure(Q, hparams, pertub)
+
+    def _configure(self, Q, hparams, pertub):
+        super()._configure(Q, hparams, verify_precision=False)
+
+        smallest_2_eigs = eigsh(Q, k=2, which='SA', return_eigenvectors=False)
+        if smallest_2_eigs[1] >= 1e-4:
+            raise ValueError('Spatial precision matrix Q must be singular.')
+        if pertub is None:
+            self.fixed.pertub = smallest_2_eigs[0]
+        else:
+            self.fixed.pertub = pertub
+
+        self.fixed.wzeros_no = np.zeros(self.fixed.W_not_obs.shape[0])
+        self.fixed.ones_no = np.ones((3, self.fixed.zeros_no.shape[0]))
+        self.dists.pg = PolyaGamma()
+        self.dists.sum2zero_mvnorm = SumToZeroMultivariateNormal()
+        self.dists.mvnorm = DenseMultivariateNormal2()
+
+    def _update_omega_a(self):
+        self.state.exists = self.state.z[self.W.surveyed].nonzero()[0]
+        self.state.W = self.W[self.state.exists]
+        b = self.state.W @ self.state.alpha
+        self.dists.pg.rvs(np.ones_like(b), b, b)
+        self.state.omega_a = b
+
+    def _update_omega_b(self):
+        b = self.X @ self.state.beta + self.state.spatial
+        self.dists.pg.rvs(self.fixed.ones, b, b)
+        self.state.omega_b = b
+
+    def _update_tau(self):
+        eta = self.state.eta
+        rate = (0.5 * eta @ self.fixed.Q @ eta) + self.fixed.tau_rate
+        self.state.tau = self.rng.gamma(self.fixed.tau_shape, 1 / rate)
+
+    def _update_eta(self):
+        A = self.fixed.Q.copy()
+        A.data = A.data * self.state.tau
+        A.setdiag(A.diagonal() + self.state.omega_b + self.fixed.pertub)
+        b = self.state.k - (self.state.omega_b * (self.X @ self.state.beta))
+        self.state.eta = self.dists.sum2zero_mvnorm.rvs(b, A)
+        self.state.spatial = self.state.eta
+
+    def _update_alpha(self):
+        W = self.state.W
+        y = self.y[self.state.exists] - 0.5
+        A = (W.T * self.state.omega_a) @ W + self.fixed.a_prec
+        b = W.T @ y + self.fixed.a_prec_by_mu
+        self.state.alpha = self.dists.mvnorm.rvs(b, A)
+
+    def _update_beta(self):
+        spat = self.state.spatial
+        omega = self.state.omega_b
+        A = (self.X.T * omega) @ self.X + self.fixed.b_prec
+        b = self.X.T @ (self.state.k - (omega * spat)) + self.fixed.b_prec_by_mu
+        self.state.beta = self.dists.mvnorm.rvs(b, A)
+
+    def _update_z(self):
+        no = self.fixed.not_obs
+        ns = self.fixed.not_surveyed
+        beta = self.state.beta
+        spat = self.state.spatial
+
+        xb_eta = self.X[no] @ beta + spat[no]
+        y = -np.logaddexp(self.fixed.zeros_no, -xb_eta)
+        w_a = self.fixed.W_not_obs @ self.state.alpha
+        omd = -np.logaddexp(self.fixed.wzeros_no, w_a)
+        split_sum(omd, self.fixed.sections, self.state.section_sums)
+        x = y + self.state.section_sums
+        c = -np.logaddexp(self.fixed.zeros_no, xb_eta)
+        logp = x - np.logaddexp(c, x)
+        self.state.z[no] = self.rng.binomial(n=1, p=np.exp(logp))
+
+        if ns:
+            xb_eta_ns = self.X[ns] @ beta + spat[ns]
+            logp = -np.logaddexp(self.fixed.zeros_ns, -xb_eta_ns)
+            self.state.z[ns] = self.rng.binomial(n=1, p=np.exp(logp))
+
+        self.state.k = self.state.z - 0.5
+
+    def step(self):
+        self._update_omega_b()
+        self._update_tau()
+        self._update_eta()
+        self._update_beta()
+        self._update_omega_a()
+        self._update_alpha()
+        self._update_z()
+
+
+class LogitRSRGibbs(LogitICARGibbs):
+    def __init__(
+        self, Q, W, X, y, hparam=None, random_state=None, r=0.5, q=None,
+    ):
+        super().__init__(Q, W, X, y, hparam, random_state)
+        self._configure_rsr(r, q)
+
+    def _configure_rsr(self, r, q):
+        # XTX_i = inv(self.X.T @ self.X)
+        chol = np.linalg.cholesky(self.X.T @ self.X)
+        z = solve_triangular(chol, np.eye(self.X.shape[1]), lower=True)
+        XTX_i = solve_triangular(chol, z, lower=True, trans=1)
+
+        # P = I - X @ XTX_i @ XT
+        P = -multi_dot([self.X, XTX_i, self.X.T])
+        P[np.diag_indices_from(P)] += 1
+
+        A = self.fixed.Q.copy()
+        A.data = -A.data
+        A.setdiag(0)
+        omega = self.fixed.n * (P.T @ A @ P) / A.sum()
+        # return eigen vectors of omega and keep first q columns
+        # corresponding to the q largest eigenvalues of omega greater
+        # than the specified threshold
+        w, v = eigh(omega, overwrite_a=True)
+        # order eigens in descending order
+        w, v = w[::-1], np.fliplr(v)
+        if q:
+            self.fixed.q = q
+        else:
+            if not 0 <= r <= 1:
+                raise ValueError('Threshold value needs to be in [0, 1]')
+            # number of eigenvalues > threshold
+            self.fixed.q = w[w >= r].size
+            if not self.fixed.q:
+                raise ValueError(
+                    'The Moran Operator Matrix of the data has no positive '
+                    'eigenvalues. Set threshold to a lower value'
+                )
+        # keep first q eigenvectors of ordered eigens
+        K = v[:, :self.fixed.q]
+        # replace Q with Minv
+        Q_copy = self.fixed.Q
+        del self.fixed.Q
+        self.fixed.Q = K.T @ Q_copy @ K
+        self.fixed.K = K
+        # `_set_default_hyperparams` has been called so modify tau_shape
+        del self.fixed.tau_shape
+        self.fixed.tau_shape = 0.5 + 0.5 * self.fixed.q
+
+    def _initialize_default_start(self, state):
+        state = super()._initialize_default_start(state)
+        state.eta = self.rng.normal(scale=5, size=self.fixed.q)
+        state.spatial = self.fixed.K @ self.state.eta
+        return state
+
+    def _initialize_posterior_state(self, start=None):
+        if start is None:
+            self.state = self._initialize_default_start(self.state)
+        else:
+            self.state.alpha = start['alpha']
+            self.state.beta = start['beta']
+            self.state.tau = start['tau']
+            self.state.eta = start['eta']
+            self.state.spatial = self.fixed.K @ self.state.eta
+
+    def _update_eta(self):
+        K = self.fixed.K
+        omega = self.state.omega_b
+        A = (K.T * omega) @ K + (self.state.tau * self.fixed.Q)
+        b = K.T @ (self.state.k - omega * (self.X @ self.state.beta))
+        self.state.eta = self.dists.mvnorm.rvs(b, A)
+        self.state.spatial = self.fixed.K @ self.state.eta
